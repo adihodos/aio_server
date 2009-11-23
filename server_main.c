b51a3d6a866fa9d489a4a74a32bf0f8edc82ba7a
Adrian Hodos <adi.hodos@gmail.com>
2009-11-23 11:15:23 +0200

    Added .gitignore file. Made server start from server_main.c

---
+++ server_main.c
@@ -0,0 +1,8 @@
+#include "server.h"
+#include "base/basetypes.h"
+#include "base/debug_helpers.h"
+
+int main(int argc, char** argv) {
+  set_unhandled_exceptions_filter();
+  return server_start_and_run(argc, argv);
+}
